2ea038e2220e43aa1918e5c58096cfe087da1818
xuchang <870819621@qq.com>
2021-09-23 21:33:49 +0800

    初步完成模板类

--- main.cpp
+++ main.cpp
@@ -1,9 +1,65 @@
-#include <iostream>
+//
+// Created by c2xu on 2021/9/17.
+//
 
-using namespace std;
+template <class T>
+class MyNode {
+public:
+    T data;
+    MyNode* nextptr;
 
-int main(void)
-{
-    cout<<"Hello World!"<<endl;
-    return 0;
+    MyNode(T data) {
+        this->data = data;
+    }
+
+    bool operator==(const MyNode& d) {
+        return data == d.data;
+    }
+
+    bool operator==(T d) {
+        return data == d;
+    }
+};
+
+template <class T>
+class MyList {
+public:
+    MyList(T data) {
+        head = new MyNode<T>(data);
+        head->nextptr = nullptr;
+    };
+
+    ~MyList() {
+        while (head) {
+            if (head->nextptr)
+            {
+                auto tmp = head;
+                head = head->nextptr;
+                delete tmp;
+            }
+        }
+    }
+
+    void insert(T data) {
+        auto node =  new MyNode<T>(data);
+        node->nextptr = head->nextptr;
+        head->nextptr = node;
+    }
+
+    void erase(T data) {
+        
+       //todo
+    }
+
+    bool isContain(T data) {
+        auto tmp = head;
+        where(tmp) {
+            //todo
+        }
+    }
+private:
+    MyNode<T>* head;
+};
+int main() {
+
 }
